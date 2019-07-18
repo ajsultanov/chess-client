f3ba9516361a2b42d14741a273f8abe4e60566b4
Adam Sultanov <adam.sultanov@gmail.com>
2019-07-17 20:10:09 -0400

    adding redux

---
+++ src/components/UserLogin.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react';
+import { connect }          from 'react-redux';
+import { createUser }       from '../actions/users'
+
+class UserLogin extends Component {
+
+  state = {
+    username: '',
+    password: ''
+  }
+
+  handleOnNameChange = event => {
+    this.setState({
+      username: event.target.value
+    });
+  }
+
+  handleOnPasswordChange = event => {
+    this.setState({
+      password: event.target.value
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={(event) => this.handleOnSubmit(event)}>
+          <p>
+            <input
+              type="text"
+              onChange={(event) => this.handleOnNameChange(event)}
+              id="username"
+              placeholder="username" />
+          </p>
+          <p>
+            <input
+              type="text"
+              onChange={(event) => this.handleOnPasswordChange(event)}
+              id="password"
+              placeholder="password" />
+          </p>
+          <input type="submit" />
+        </form>
+      </div>
+    );
+  }
+
+}
+
+export default connect(null, { createUser })(UserLogin)
